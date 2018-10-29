0237349c55bf97373ce8e1a0c42b253b49151df5
volkov-v <volkov.vladyslav.2018@gmail.com>
2018-10-29 12:42:16 +0200

    modified homework2

--- homework2/homework2.js
+++ homework2/homework2.js
@@ -1,12 +1,8 @@
-function min_number(arr)
-{
+function min_number(arr){
   var min=arr[0]
-  for (var i=1; i<arr.length; i++)
-  {
-    if (arr[i]!=null && arr[i]!=undefined && arr[i]!=Infinity && !isNaN(arr[i]) && arr[i]!=-Infinity)
-    {
-      if (arr[i]<min)
-      {
+  for (var i=1; i<arr.length; i++) {
+    if (arr[i] != null && arr[i] != undefined && isFinite(arr[i])) {
+      if (arr[i]<min) {
         min=arr[i]
       }
     }
@@ -14,15 +10,11 @@ function min_number(arr)
   return min;
 }
 
-function max_number(arr)
-{
+function max_number(arr){
   var max=arr[0];
-  for (var i=1; i<arr.length; i++)
-  {
-    if (arr[i]!=null && arr[i]!=undefined && arr[i]!=Infinity && !isNaN(arr[i]) && arr[i]!=-Infinity)
-    {
-      if (arr[i]>max)
-      {
+  for (var i=1; i<arr.length; i++) {
+    if (arr[i] != null && arr[i] != undefined && isFinite(arr[i])) {
+      if (arr[i]>max) {
         max=arr[i];
       }
     }
@@ -30,38 +22,36 @@ function max_number(arr)
   return max;
 }
 
-function sum_number(arr)
-{
+function sum_number(arr) {
   var sum=0;
-  for (var i=0; i<arr.length; i++)
-  {
-    if (arr[i]!=null && arr[i]!=undefined && arr[i]!=Infinity && !isNaN(arr[i]) && arr[i]!=-Infinity)
-    {
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i] != null && arr[i] != undefined && isFinite(arr[i])) {
       sum+=arr[i];
     }
   }
   return sum;
 }
 
-var initial_array;
+function min_max_sum_array(function_name, arr) {
+  var result;
+  switch(function_name) {
+    case 'min':
+      result = min_number(arr);
+      break;
+    case 'max':
+      result = max_number(arr);
+      break;
+    case 'sum':
+      result = sum_number(arr);
+      break;
+  }
+  return result;
+}
+
 
 initial_array=[3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-console.log(`Minimal value of array [${initial_array}] is ${min_number(initial_array)}`);
-console.log(`Maximal value of array [${initial_array}] is ${max_number(initial_array)}`);
-console.log(`Sum of values of array [${initial_array}] is ${sum_number(initial_array)}`);
+console.log(`Minimal value of array [${initial_array}] is ${min_max_sum_array('min', initial_array)}`);
 initial_array=[-1,-8,-2];
-console.log(`Minimal value of array [${initial_array}] is ${min_number(initial_array)}`);
-console.log(`Maximal value of array [${initial_array}] is ${max_number(initial_array)}`);
-console.log(`Sum of values of array [${initial_array}] is ${sum_number(initial_array)}`);
-initial_array=[1,7,3];
-console.log(`Minimal value of array [${initial_array}] is ${min_number(initial_array)}`);
-console.log(`Maximal value of array [${initial_array}] is ${max_number(initial_array)}`);
-console.log(`Sum of values of array [${initial_array}] is ${sum_number(initial_array)}`);
+console.log(`Maximal value of array [${initial_array}] is ${min_max_sum_array('max', initial_array)}`);
 initial_array=[1,undefined,3,5,-3];
-console.log(`Minimal value of array [${initial_array}] is ${min_number(initial_array)}`);
-console.log(`Maximal value of array [${initial_array}] is ${max_number(initial_array)}`);
-console.log(`Sum of values of array [${initial_array}] is ${sum_number(initial_array)}`);
-initial_array=[1,NaN,3,5,-3];
-console.log(`Minimal value of array [${initial_array}] is ${min_number(initial_array)}`);
-console.log(`Maximal value of array [${initial_array}] is ${max_number(initial_array)}`);
-console.log(`Sum of values of array [${initial_array}] is ${sum_number(initial_array)}`);
+console.log(`Sum of values of array [${initial_array}] is ${min_max_sum_array('sum', initial_array)}`);
