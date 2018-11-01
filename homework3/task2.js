ee79767bd3fed74ef16f8a8c64b910cbf0e3a597
volkov-v <volkov.vladyslav.2018@gmail.com>
2018-11-01 09:43:29 +0200

    Changes in task2 of homework3 for duplicate values in array

--- homework3/task2.js
+++ homework3/task2.js
@@ -1,8 +1,19 @@
 function arrayToObject (arr) {
   var obj = {}
   arr.forEach(function (item, i, arr) {
-    obj[item] = i
+    if (item in obj) {
+      if (Array.isArray(obj[item])) {
+        obj[item].push(i)
+      } else {
+        let arr = []
+        arr.push(obj[item])
+        arr.push(i)
+        obj[item] = arr
+      }
+    } else {
+      obj[item] = i
+    }
   })
   return obj
 }
-console.log(arrayToObject([2, 'apple', 5, 0, true, 4, 7, null, 3, 1, -2, undefined]))
+console.log(arrayToObject([2, 'apple', 5, 2, 2, 0, true, 4, 7, 5, null, 3, 1, -2, undefined]))
